2e9a7239ddaacb05fe4a60a760b0f06c8ae7229b
codeslug <crazyshannon@gmail.comm>
2023-07-06 01:08:31 -0700

    removed components

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          I love <a href="https://nextjs.org">All the Slugs!!</a>
+          I love <a href="https://nextjs.org">All the Slugs and Snails!!</a>
         </h1>
 
         <p className={styles.description}>
