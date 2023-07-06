1b100ba9ef8160a9fe30cd4d9a8351f5645fd95e
codeslug <crazyshannon@gmail.comm>
2023-07-06 00:54:04 -0700

    remove button text

--- pages/index.js
+++ pages/index.js
@@ -17,42 +17,6 @@ export default function Home() {
         </h1>
 
 
-        export default function Home() {
-          const [message, setMessage] = useState('');
-
-          const handleSendMessage = async () => {
-            try {
-              const response = await fetch('https://www.hackercoop.dev/api/boop', {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ message }),
-              });
-
-              if (response.ok) {
-                console.log('Message sent successfully!');
-              } else {
-                console.log('Failed to send message!');
-              }
-            } catch (error) {
-              console.error('An error occurred while sending the message:', error);
-            }
-          };
-
-          const handleChangeMessage = (event) => {
-            setMessage(event.target.value);
-          };
-
-          return (
-            <div>
-              <h1>Discord Message Sender</h1>
-              <input type="text" value={message} onChange={handleChangeMessage} />
-              <button onClick={handleSendMessage}>Send Message</button>
-            </div>
-          );
-        }
-
 
 
       </main>
