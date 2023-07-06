a65b4b60dc4b4042c483d33bb77130b070098e60
codeslug <crazyshannon@gmail.comm>
2023-07-05 22:20:16 -0700

    slug text added

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Learn <a href="https://nextjs.org">Next.js!</a>
+          I love <a href="https://nextjs.org">Slugs!</a>
         </h1>
 
         <p className={styles.description}>
